a3f33af5371600a3728968622c4013bc48ddf451
sagarsajwan26 <sagarsajwan26@gmail.com>
2025-07-06 22:55:24 +0530

    Qr project commit

--- api/app.js
+++ api/app.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 const express= require('express')
 const bodyParser = require('body-parser')
 const cors=require('cors')
-const db= require('./db')
+const db= require('../db')
+const serverless= require('serverless-http')
 const app= express()
 
 
-const userModel= require('./model/User')
 
-const userRouter=require('./routes/UserRoutes')
+const userRouter=require('../routes/UserRoutes')
 
 
 app.use(bodyParser.json())
@@ -20,10 +20,6 @@ app.use(cors({
 
 app.use('/userapi',userRouter)
 
+module.exports= app
 
-
-const PORT= process.env.PORT
-app.listen(PORT,()=>{
-    console.log('server started',PORT);
-    
-})
+module.exports.handler = serverless(app)
